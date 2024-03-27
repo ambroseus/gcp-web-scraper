e9481bd6bc9c7a14052be28f14226cd6da7e3fd0
Eugene Samonenko <ambroseus@gmail.com>
2024-03-27 14:32:46 +0200

    added mantine table

--- functions/admin/app/layout.tsx
+++ functions/admin/app/layout.tsx
@@ -9,8 +9,8 @@ import '@mantine/dates/styles.css'
 import 'mantine-react-table/styles.css'
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'web-scraper admin',
+  // description: 'I am using Mantine with Next.js!',
 }
 
 export default function RootLayout({ children }: { children: any }) {
--- functions/admin/components/QueryProvider/index.tsx
+++ functions/admin/components/QueryProvider/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export const QueryProvider = ({ children }) => {
-  const [client] = useState(new QueryClient())
+  const [client] = useState(new QueryClient()) // workaround for Next.js 14
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>
 }
