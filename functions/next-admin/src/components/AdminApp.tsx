afbfec7552f1725709c8fa69c3095ce05c8310f4
Eugene Samonenko <ambroseus@gmail.com>
2024-06-12 16:57:22 +0300

    use @dckit/ra-firebase

--- functions/next-admin/src/components/AdminApp.tsx
+++ functions/next-admin/src/components/AdminApp.tsx
@@ -2,16 +2,7 @@
 import * as React from 'react'
 import { EventList, EventShow, EventCreate, EventEdit } from './events'
 import { Admin, Resource } from 'react-admin'
-import { FirebaseDataProvider, FirebaseAuthProvider, RAFirebaseOptions } from 'react-admin-firebase'
-import { firebaseConfig as config } from '../api/credentials'
-
-const options: RAFirebaseOptions = {
-  logging: true,
-  rootRef: '/',
-  watch: ['events'],
-}
-const dataProvider = FirebaseDataProvider(config, options)
-const authProvider = FirebaseAuthProvider(config, options)
+import { dataProvider, authProvider } from '@/api'
 
 const AdminApp = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
