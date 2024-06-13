822d8e7b84857efe9e2a25693aa2eec63c84bc7e
Eugene Samonenko <ambroseus@gmail.com>
2024-06-13 15:36:33 +0300

    api auth

--- functions/next-admin/src/firebase/index.ts
+++ functions/next-admin/src/firebase/index.ts
@@ -9,3 +9,15 @@ const options: RAFirebaseOptions = {
 
 export const dataProvider = FirebaseDataProvider(config, options)
 export const authProvider = FirebaseAuthProvider(config, options)
+
+export const resUnauthorized = () => new Response('Unauthorized', { status: 401 })
+
+export const authApiRequest = async (req: Request): Promise<boolean> => {
+  const apiKey = req.headers.get('api-key')
+  const user = req.headers.get('user')
+  if (!apiKey || !user) return false
+
+  const api = await dataProvider.getOne('api', { id: user })
+
+  return apiKey === api?.data?.key
+}
